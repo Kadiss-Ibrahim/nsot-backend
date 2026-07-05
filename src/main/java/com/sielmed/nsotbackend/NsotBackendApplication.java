2ba66f2c79220a45ad7b6c11e0a322bd4fc5e6e2
Kadiss Ibrahim <ibrahimaitkadiss@gmail.com>
2026-07-05 20:06:05 +0100

    Creation of the project and the mvc structure

---
+++ src/main/java/com/sielmed/nsotbackend/NsotBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sielmed.nsotbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NsotBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NsotBackendApplication.class, args);
+	}
+
+}
